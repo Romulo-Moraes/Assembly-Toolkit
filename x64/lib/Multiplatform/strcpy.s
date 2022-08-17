2375722fc04462ce68b54bbaefd0c9e4a661959a
Romulo-Moraes <romuloperes6@gmail.com>
2022-08-17 03:43:10 +0000

    strcpy cross-platform x86_64 created

---
+++ x64/lib/Multiplatform/strcpy.s
@@ -0,0 +1,25 @@
+;Copy a string from source address to destiny address
+
+; Destiny address in RDI, source address in RSI
+
+strcpy:
+    push rdi
+    push rsi
+
+while:
+    cmp [rsi], byte 0
+    je end
+
+    mov r8b, [rsi]
+    mov [rdi], r8b
+    inc rdi
+    inc rsi
+
+    jmp while
+end:
+    mov [rdi], byte 0
+
+    pop rsi
+    pop rdi
+
+    ret
