917ef987a6f911b84e28bab61a73ece6c9c7457e
Romulo-Moraes <romuloperes6@gmail.com>
2023-04-02 17:11:44 +0000

    memset finished

--- 9. Solutions/README.md
+++ 9. Solutions/README.md
@@ -6,3 +6,6 @@ strcpy is a function to copy a string from a source to a destiny, this works cop
 
 ## 9.2 strlen
 strlen is a function to calculate the size of a string in memory, the calculation keep going until the function face with a NULL byte. The **address** of the string must be passed to RDI register. The function returns an integer meaning the size of the string in RAX register. The function doesn't count the NULL byte as part of the string size.
+
+## 9.3 memset
+memset is a function that fills the first x bytes pointed by an address, with the value y. The **address** to fill must be passed to RDI register, the **byte** used to fill the area must be passed to RSI register, the **number** of bytes to fill with the value must be passed to RDX register. The function returns the address of the filled area in RAX register.
---
+++ 9. Solutions/src/memset.asm
@@ -0,0 +1,53 @@
+; memset - Function that fill an area in memory with a byte
+;
+; RDI register is used as address to be filled
+; RSI register is used as the byte to fill the area
+; RDX register is used as the number of bytes to fill
+; RAX is the return value (contains the address of the filled area)
+;
+; The function doesn't change the value of RDI, RSI and RDX
+
+%define __MEMSET_COPY_COUNTER -8
+
+memset:
+    ; Save some registers into stack
+    push rbp
+    push rdi
+
+    ; Allocate memory in stack frame
+    mov rbp, rsp
+    sub rsp, 8
+
+    ; Put zero inside the counter variable
+    mov QWORD [rbp+__MEMSET_COPY_COUNTER], 0
+memset_loop:
+    ; Compare whether the counter still
+    ; less than the value specified by 
+    ; the programmer
+    cmp [rbp+__MEMSET_COPY_COUNTER], rdx
+
+    ; If not, jump to end
+    jge memset_end
+
+    ; If yes, copy the byte to the address
+    mov [rdi], sil
+
+    ; Increment counter and the address
+    inc QWORD [rbp+__MEMSET_COPY_COUNTER]
+    inc rdi
+
+    ; Jump to the loop again
+    jmp memset_loop
+memset_end:
+    ; Deallocate memory in stack rame
+    add rsp, 8
+
+    ; Restore registers previously
+    ; pushed into stack
+    pop rdi
+    pop rbp
+
+    ; Copy address of the filled area
+    ; to return value register
+    mov rax, rdi
+    ret
