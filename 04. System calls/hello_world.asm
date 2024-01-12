eece11f82c17408e28be7f9783a6ac53dbfa3906
Romulo-Moraes <romuloperes6@gmail.com>
2024-01-12 15:14:06 -0300

    Update System calls section

--- 04. System calls/hello_world.asm
+++ 04. System calls/hello_world.asm
@@ -1,72 +1,71 @@
-; Below the text segment is where we will put 
-; the instructions of our software.
-; the 'global' keyword is used to tell the 
-; linker what procedures you wan't to make
-; available across object files, and is also
-; used to tell the linker where our program
-; starts
+; As mentioned before, the text segment is used to
+; store the program instructions, so is within this 
+; segment that we are going to write the Assembly code
+
+; The global keyword is used to make a symbol available
+; across all object files. The linker requires that the
+; main text symbol be visible using this statement.
+global _start
 
 segment .text
-    ; 'global _start' is always required
-    ; whether you wan't make this file
-    ; executable
-    global _start
 
-; Entry point of our program. The linker
-; always waits for the procedure name _start
-; as entry point by default.
+; By default, the _start is the entry point of the program.
 _start:
 
     ; The following block of code will print a
-    ; message on terminal and each register
-    ; has a meaning. the rax represent the 
-    ; operation to do, the programmers that   
-    ; built the linux made 1 represents 
-    ; print on terminal, rdi is the file 
-    ; descriptor to do it, being '0' the 
-    ; STDIN, '1' the STDOUT, and '2' the 
-    ; STDERR, rsi is the pointer to the 
-    ; message and rdx is the size of the 
-    ; message to be printed.
+    ; message on terminal.
+    ;
+    ; The rax register means the operation to be performed, 
+    ; the programmers that built the linux made 
+    ; the value 1 represent "print something...".
+    ;
+    ; The rdi register is the file descriptor to where the print
+    ; shall be redirected, '0' represents the STDIN,
+    ; '1' the STDOUT, and '2' the STDERR.
+    ;
+    ; The rsi register is the pointer to the 
+    ; message.
+    ;
+    ; The rdx register is the message length
 
     mov rax, 1
     mov rdi, 1
     mov rsi, message
     mov rdx, message_size
 
-    ; After filling the registers with the 
-    ; correct values, just send a message to 
-    ; the OS asking for a favor
+    ; After setting the registers with the
+    ; correct values, ask the OS to perform the
+    ; task
     syscall
 
-    ; Other favor asking, but this time is 
-    ; saying to the OS that our program 
-    ; finished all its instructions and wan't 
-    ; be unloaded from memory. the rdi 
-    ; register is the status that will be sent 
-    ; to the OS on program exit
+    ; Our program finished all its instructions,
+    ; so we need to ask the OS to unload the
+    ; program from memory    
+    ;
+    ; 60 is the exit syscall
+    ; 0 is the exit status
 
     mov rax, 60
     mov rdi, 0
 
-    ; Asking the favor
+    ; Asking the OS to perform the task
     syscall
 
 ; The read-only data segment is the place
-; where we put the 'Hello, world!' message.
+; where we define the 'Hello, world!' message.
 segment .rodata
 
-    ; We use 'db' that means define byte to 
-    ; define a byte or a chain of bytes.
-    ; each comma outside the quotes is a 
-    ; concatenation, then there was added
-    ; 0xa that is the decimal character 10,
-    ; representing '\n' and finally the null 
-    ; byte that is always necessary when we
+    ; We use 'db' to Define a Byte or a chain of bytes.
+    ; Every comma outside the quotes is a 
+    ; concatenation, so the value 0xa, that means '\n'
+    ; is appended to the string.
+    ;
+    ; The 0x0 represents the null 
+    ; character, that is always necessary when we
     ; are handling with strings
 
     message db 'Hello, world!', 0xa, 0x0
 
-    ; 'equ $-' here means to get the size of a 
-    ; constant, then message_size = sizeof(message)
+    ; 'equ $-' means to get the size of a 
+    ; constant, so message_size = sizeof(message)
     message_size equ $- message
